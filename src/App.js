259b3a2182d0f30d0aae29f4579526916ef160fa
Shubham <shubhamshahrising@gmail.com>
2020-08-08 11:24:46 +0530

    feat: print bangalore map in svg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import * as d3 from 'd3'
+
 import './App.css'
 
+import BangaloreRoutes from './data/routes.json'
+import BangaloreBusStops from './data/bus_stops.json'
+import BangaloreSchools from './data/schools.json'
+import BangaloreBoundaries from './data/bangalore_boundaries.json'
+
 function App() {
+  const [dimensions, setDimensions] = useState({ width: 600, height: 600 })
+  const { width, height } = dimensions
+
+  useEffect(() => {
+    var projection = d3
+      .geoMercator()
+      .translate([width / 2, height / 2])
+      .scale(Math.pow(10, 5))
+      .center([77.624566, 12.981599])
+
+    var geoGenerator = d3.geoPath().projection(projection)
+
+    // Join the FeatureCollection's features array to path elements
+    var u = d3
+      .select('svg')
+      .selectAll('path')
+      .data(BangaloreBoundaries.features)
+
+    // Create path elements and update the d attribute using the geo generator
+    u.enter().append('path').attr('d', geoGenerator)
+  }, [width, height])
   return (
-    <div className="App">
-      <svg className="map-svg">
-        <circle cy="10" r="10" cx="10"></circle>
-      </svg>
-    </div>
+    <>
+      <svg width={width} height={height}></svg>
+    </>
   )
 }
 
---
+++ src/useResizeObserver.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
+
+/**
+ * Hook, that returns the current dimensions of an HTML element.
+ * Doesn't play well with SVG.
+ */
+
+const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null)
+  useEffect(() => {
+    const observeTarget = ref.current
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect)
+      })
+    })
+    resizeObserver.observe(observeTarget)
+    return () => {
+      resizeObserver.unobserve(observeTarget)
+    }
+  }, [ref])
+  // use resized dimensions
+  // but fall back to getBoundingClientRect, if no dimensions yet.
+
+  return (
+    dimensions ||
+    (ref.current && ref.current.getBoundingClientRect()) || {
+      width: 0,
+      height: 0,
+    }
+  )
+}
+
+export default useResizeObserver
