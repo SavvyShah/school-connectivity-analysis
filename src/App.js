0c6b29438282c47d25647f550eda0b24104194d0
Shubham <shubhamshahrising@gmail.com>
2020-08-10 09:01:38 +0530

    refactor: chloropleth logic to new file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import * as d3 from 'd3'
+import React from 'react'
 
 import './App.css'
-import BusStops from './data/bus_stops_in_region.json'
 
 import useBangaloreMap from './hooks/useBangaloreMap'
-import cleanClassAndID from './utils/cleanClassAndID'
-import maxValueInRegion from './utils/maxValueInRegion'
-import Modal from './components/Modal'
+import useChloropleth from './hooks/useChloropleth'
 
 function App() {
   const dimensions = {
@@ -21,59 +17,7 @@ function App() {
     width: width,
     height: height,
   })
-  useEffect(() => {
-    if (d3.select('g>path')) {
-      const maxBusStops = maxValueInRegion(BusStops)
-      const colorScale = d3
-        .scaleLinear()
-        .domain([0, maxBusStops])
-        .range(['rgb(0,200,200)', 'rgb(0,0,100)'])
-
-      for (let region in BusStops) {
-        d3.selectAll(`.${cleanClassAndID(region)}`)
-          .attr('fill', () => {
-            return colorScale(BusStops[region].length)
-          })
-          .attr('stroke', () => {
-            return colorScale(BusStops[region].length)
-          })
-      }
-      d3.select('svg')
-        .append('g')
-        .selectAll('rect')
-        .data([
-          0,
-          maxBusStops / 4,
-          maxBusStops / 2,
-          (3 * maxBusStops) / 4,
-          maxBusStops,
-        ])
-        .enter()
-        .append('rect')
-        .attr('class', 'color-scale-rect')
-        .attr('fill', (d) => colorScale(d))
-        .attr('x', (d, i) => 50 * i + width - 500)
-        .attr('y', 10)
-        .attr('width', 50)
-        .attr('height', 10)
-      d3.select('svg')
-        .append('g')
-        .selectAll('text')
-        .data([
-          0,
-          maxBusStops / 4,
-          maxBusStops / 2,
-          (3 * maxBusStops) / 4,
-          maxBusStops,
-        ])
-        .enter()
-        .append('text')
-        .attr('x', (d, i) => 50 * i + width - 500)
-        .attr('y', 33)
-        .attr('fill', 'black')
-        .text((d) => '|' + d)
-    }
-  })
+  useChloropleth(width)
   return (
     <>
       <h1>Distribution of bus stops in Bangalore</h1>
---
+++ src/hooks/useChloropleth.js
@@ -0,0 +1,67 @@
+import { useEffect } from 'react'
+import * as d3 from 'd3'
+
+import BusStops from '../data/bus_stops_in_region.json'
+
+import cleanClassAndID from '../utils/cleanClassAndID'
+import maxValueInRegion from '../utils/maxValueInRegion'
+
+function useChloropleth(width) {
+  useEffect(() => {
+    if (d3.select('g>path')) {
+      const maxBusStops = maxValueInRegion(BusStops)
+      const colorScale = d3
+        .scaleLinear()
+        .domain([0, maxBusStops])
+        .range(['rgb(0,200,200)', 'rgb(0,0,100)'])
+
+      for (let region in BusStops) {
+        d3.selectAll(`.${cleanClassAndID(region)}`)
+          .attr('fill', () => {
+            return colorScale(BusStops[region].length)
+          })
+          .attr('stroke', () => {
+            return colorScale(BusStops[region].length)
+          })
+      }
+      d3.select('svg')
+        .append('g')
+        .selectAll('rect')
+        .data([
+          0,
+          maxBusStops / 4,
+          maxBusStops / 2,
+          (3 * maxBusStops) / 4,
+          maxBusStops,
+        ])
+        .enter()
+        .append('rect')
+        .attr('class', 'color-scale-rect')
+        .attr('fill', (d) => colorScale(d))
+        .attr('x', (d, i) => 50 * i + width - 500)
+        .attr('y', 10)
+        .attr('width', 50)
+        .attr('height', 10)
+      d3.select('svg')
+        .append('g')
+        .selectAll('text')
+        .data([
+          0,
+          maxBusStops / 4,
+          maxBusStops / 2,
+          (3 * maxBusStops) / 4,
+          maxBusStops,
+        ])
+        .enter()
+        .append('text')
+        .attr('x', (d, i) => 50 * i + width - 500)
+        .attr('y', 33)
+        .attr('fill', 'black')
+        .text((d) => '|' + d)
+      d3.select('svg').on('click', function () {
+        d3.select(this)
+      })
+    }
+  }, [width])
+}
+export default useChloropleth
